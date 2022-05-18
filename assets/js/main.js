881408fe535db6e39dcbb8e233b688cf31d38909
zevik7 <zevik2807@gmail.com>
2022-05-18 17:55:05 +0700

    feat: update animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,33 +2,59 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 // Navigation responsive
-const navToggle = document.querySelector(".mobile-btn");
-const navBox = document.querySelector(".nav-box");
-const navBack = document.querySelector(".nav-box__responsive-background");
+const navToggle = document.querySelector('.mobile-btn');
+const navBox = document.querySelector('.nav-box');
+const navBack = document.querySelector('.nav-box__responsive-background');
 
+// Events for nav responsive
 navToggle.onclick = () => {
-	navBox.className = "nav-box responsive";
+  navBox.className = 'nav-box responsive';
 };
 navBack.onclick = () => {
-	navBox.className = "nav-box";
+  navBox.className = 'nav-box';
 };
 
 // Animation when scrolling
-const demosItems = $$(".demos-item");
+const demosItems = $$('.demos-item');
+const header = $('.header');
+const bg = $('.ecommerce .active');
+const frontBg = $('.ecommerce .front-bg');
 
 function listenScroll() {
-	for (var i = 0; i < demosItems.length; i++) {
-		var windowHeight = window.innerHeight;
-		var elementTop = demosItems[i].getBoundingClientRect().top;
-		var elementVisible = 150;
+  const elementVisible = 150;
+  const windowHeight = window.innerHeight;
 
-		if (elementTop < windowHeight - elementVisible) {
-			demosItems[i].classList.add("active");
-		} else {
-			demosItems[i].classList.remove("active");
-		}
-	}
+  // Demos items
+  for (var i = 0; i < demosItems.length; i++) {
+    const demoTop = demosItems[i].getBoundingClientRect().top;
+    if (demoTop < windowHeight - elementVisible) {
+      demosItems[i].classList.add('active');
+    } else {
+      demosItems[i].classList.remove('active');
+    }
+  }
+
+  // Header
+  if (window.scrollY > 300) {
+    header.classList.add('fixed');
+  } else {
+    header.classList.remove('fixed');
+  }
+
+  // Background
+  const bgTop = bg.getBoundingClientRect().top;
+  const frontBgTop = bg.getBoundingClientRect().top;
+  if (bgTop < windowHeight - elementVisible) {
+    bg.classList.add('active');
+  } else {
+    bg.classList.remove('active');
+  }
+  if (frontBgTop < windowHeight - elementVisible) {
+    frontBg.classList.add('active');
+  } else {
+    frontBg.classList.remove('active');
+  }
 }
 
-window.addEventListener("scroll", listenScroll);
-listenScroll();
+window.addEventListener('scroll', listenScroll);
+listenScroll(); // When refresh page
