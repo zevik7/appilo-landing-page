13194acffa3f270869503c74d5909aee160fe175
zevik7 <zevik2807@gmail.com>
2022-05-18 11:56:44 +0700

    feat: add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "jsxBracketSameLine": false,
+  "printWidth": 100,
+  "semi": true,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "es5",
+  "useTabs": false
+};
